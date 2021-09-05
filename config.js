f3a21d80ecc5cb36f6bc99a8890ee38eef95d795
thesergioliveira <thesergioliveira@gmail.com>
2021-09-05 22:37:40 +0200

    adding config.js and oxford-client.js with api headers and axios fetch

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  APP_ID: "24ee4b9c",
+  APP_KEY: "9287eadadf9dd24c22a849e65a1a2fd2",
+};
--- index.js
+++ index.js
@@ -1,18 +1,8 @@
-require("dotenv").config();
-const PORT = process.env.PORT | 8000;
-const express = require("express");
-const { send } = require("process");
-const app = express();
+#!/usr/bin/env node
 
-// http:localhost:5000/
-app.get("/", (req, res) => {
-  res.send("Hey, I am a home page");
-});
-// http:localhost:5000/about
-app.get("/about", (req, res) => {
-  res.send("<h1>About page<h1>");
-});
+// const dictionary = require("./lib/dictionary");
+const args = process.argv.slice(2);
+const [word] = args;
+console.log(word);
 
-app.listen(PORT, () => {
-  console.log("this is a live server application");
-});
+// dictionary(word).then(console.log).catch(console.error);
