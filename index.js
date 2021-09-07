a7cd36a86f94cf088c63ec5f297b80af9e12fb5d
thesergioliveira <thesergioliveira@gmail.com>
2021-09-07 13:51:27 +0200

    api fetch

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-#!/usr/bin/env node
-
-// const dictionary = require("./lib/dictionary");
-const args = process.argv.slice(2);
-const [word] = args;
-console.log(word);
-
+#!/usr/bin/env node // const dictionary = require("./lib/dictionary");
+const word = process.argv[2];
+// const [word] = args;
+// console.log(word);
+require("./lib/oxford-client");
+module.exports = { word };
 // dictionary(word).then(console.log).catch(console.error);
